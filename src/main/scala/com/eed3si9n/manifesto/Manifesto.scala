9cc85e264e57af14e22c1a12d3c89fb43d1861ea
Eugene Yokota <eed3si9n@gmail.com>
2024-11-02 01:37:50 -0400

    Use concrete type to avoid anonymous class

--- src/main/scala/com/eed3si9n/manifesto/Manifesto.scala
+++ src/main/scala/com/eed3si9n/manifesto/Manifesto.scala
@@ -1,6 +1,6 @@
 package com.eed3si9n.manifesto
 
-trait Manifesto[A1]:
+sealed trait Manifesto[A1]:
   def typeCon: String
   def typeArguments: List[Manifesto[?]]
   def isSingleton: Boolean
@@ -22,4 +22,21 @@ end Manifesto
 object Manifesto:
   def apply[A1: Manifesto]: Manifesto[A1] = summon[Manifesto[A1]]
   inline given [A1]: Manifesto[A1] = Derivation.derived[A1]
+
+  class Applied[A1](
+      typeCon0: String,
+      args0: List[Manifesto[?]]) extends Manifesto[A1]:
+    override val typeCon = typeCon0
+    override val typeArguments = args0
+    override val isSingleton = false
+
+  class Singleton[A1](typeName: String) extends Manifesto[A1]:
+    override val typeCon = typeName
+    override val typeArguments = Nil
+    override val isSingleton = true
+
+  class Fallback[A1](typeName: String) extends Manifesto[A1]:
+    override val typeCon = typeName
+    override val typeArguments = Nil
+    override val isSingleton = false
 end Manifesto
